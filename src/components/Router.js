8d0f72a916cee6d564d8f16feb383a50d01110fc
최준혁 <choijunhyeok@choejunhyeog-ui-MacBookPro.local>
2022-11-13 16:49:02 +0900

    Feat : Navigation

--- src/components/Router.js
+++ src/components/Router.js
@@ -2,13 +2,19 @@ import React from "react";
 import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Auth from "../routes/Auth";
 import Home from "../routes/Home";
+import Profile from "../routes/Profile";
+import Navigation from "./Navigation";
 
 const AppRouter = ({ isLoggedIn }) => {
     return(
         <Router>
+            {isLoggedIn && <Navigation />}
             <Routes>
                 {isLoggedIn ? (
-                    <Route exact path="/" element={ <Home /> } />
+                    <>
+                        <Route exact path="/" element={ <Home /> } />,
+                        <Route exact path="/profile" element={ <Profile /> } />
+                    </>
                 ) : (
                     <Route exact path="/" element={ <Auth /> } />
                 )}
