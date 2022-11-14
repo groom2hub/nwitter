fb851d4a7093112c245aef7a30d893ab0312daba
최준혁 <choijunhyeok@choejunhyeog-ui-MacBookPro.local>
2022-11-14 13:11:37 +0900

    Feat : Add data in Firestore

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,3 +1,40 @@
-const Home = () => <span>Home</span>
+import { dbService } from "../fbase";
+import { useEffect, useState } from "react";
+import { collection, getDocs, addDoc } from "firebase/firestore"
+import { async } from "@firebase/util";
+
+const Home = () => {
+    const [nweet, setNweet] = useState("");
+
+    const onSubmit = async (event) => {
+        event.preventDefault();
+        await addDoc(collection(dbService, "nweet"), {
+            text: nweet,
+            createAt: Date.now(),
+        });
+        setNweet("");
+    };
+
+    const onChange = (event) => {
+        event.preventDefault();
+        const {
+            target: { value },
+        } = event;
+        setNweet(value);
+    };
+
+    return (
+        <form onSubmit={onSubmit}>
+            <input
+                value={nweet}
+                onChange={onChange}
+                type="text"
+                placeholder="What's on your mind?"
+                maxLength={120}
+            />
+            <input type="submit" value="Nweet" />
+        </form>
+    );
+}
 
 export default Home;
