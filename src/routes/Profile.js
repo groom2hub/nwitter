740cc5c9623694ccbb05bf7133f79f13e0d05ee9
groom2hub <groomwnsgur35@gmail.com>
2022-11-15 12:41:23 +0900

    Fix : Navigate URL

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -2,11 +2,13 @@ import { authService } from "../fbase";
 import { useNavigate }from "react-router-dom";
 
 const Profile = () => {
-    const History = useNavigate();
+    const history = useNavigate();
 
     const onLogOutClick = () => {
+        //LogOut
         authService.signOut();
-        History.pushState("/");
+        //초기화면으로 이동
+        history("/");
     };
 
     return (
