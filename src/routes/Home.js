a4faf1252f94bc4b3f472b87eed6a1be96a1dcc6
groom2hub <groomwnsgur35@gmail.com>
2022-11-15 13:12:41 +0900

    Feat : Read file, Clear file

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -5,71 +5,90 @@ import Nweet from "../components/Nweet";
 import { async } from "@firebase/util";
 
 const Home = ({ userObj }) => {
-    const [nweet, setNweet] = useState("");
-    const [nweets, setNweets] = useState([]);
+  const [nweet, setNweet] = useState("");
+  const [nweets, setNweets] = useState([]);
+  const [attachment, setAttachment] =useState("");
 
-    // const getNweets = async () => {
-    //     const dbNweets = await getDocs(collection(dbService, "nweets"));
-    //     dbNweets.forEach((document) =>{
-    //         const nweetObject = { ...document.data(), id: document.id };
-    //         setNweets((prev) => [nweetObject, ...prev])
-    //     });
-    // };
+  // const getNweets = async () => {
+  //     const dbNweets = await getDocs(collection(dbService, "nweets"));
+  //     dbNweets.forEach((document) =>{
+  //         const nweetObject = { ...document.data(), id: document.id };
+  //         setNweets((prev) => [nweetObject, ...prev])
+  //     });
+  // };
 
-    //데이터베이스에서 불러오기
-    useEffect(() => {
-        // getNweets();
+  //데이터베이스에서 불러오기
+  useEffect(() => {
+    // getNweets();
 
-        // onSnapshot(collection(dbService, "sweets"), (snapshot) => { 
-        //     const newArray = snapshot.docs.map((document) => ({
-        //         id: document.id,
-        //         ...document.data(),
-        //     }));
-        //     setNweets(newArray);
-        // });
+    // onSnapshot(collection(dbService, "sweets"), (snapshot) => { 
+    //     const newArray = snapshot.docs.map((document) => ({
+    //         id: document.id,
+    //         ...document.data(),
+    //     }));
+    //     setNweets(newArray);
+    // });
 
-        const q = query(
-          collection(dbService, "nweets"),
-          orderBy("createdAt", "desc")
-        );
+    const q = query(
+      collection(dbService, "nweets"),
+      orderBy("createdAt", "desc")
+    );
     
-        const unsubscribe = onSnapshot(q, (querySnapshot) => {
-          const nextNweets = querySnapshot.docs.map((doc) => {
-            return {
-              id: doc.id, ...doc.data(),
-            };
-          });
-          setNweets(nextNweets);
-        });
-    
-        return () => {
-          unsubscribe();
+    const unsubscribe = onSnapshot(q, (querySnapshot) => {
+      const nextNweets = querySnapshot.docs.map((doc) => {
+        return {
+          id: doc.id, ...doc.data(),
         };
-      }, []);
-
-    // 저장하는 버튼
-    const onSubmit = async (event) => {
-        event.preventDefault();
-        const docRef = await addDoc(collection(dbService, "nweets"), {
-            text: nweet,
-            createdAt: serverTimestamp(),
-            creatorId: userObj.uid,
-          });
-          console.log("Document written with ID:", docRef.id);
-      
-          setNweet("");
+      });
+      setNweets(nextNweets);
+    });
+    
+    return () => {
+      unsubscribe();
     };
+  }, []);
+
+  // 저장하는 버튼
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    const docRef = await addDoc(collection(dbService, "nweets"), {
+      text: nweet,
+      createdAt: serverTimestamp(),
+      creatorId: userObj.uid,
+    });
+    console.log("Document written with ID:", docRef.id);
+    setNweet("");
+  };
       
 
-    const onChange = (event) => {
-        event.preventDefault();
-        const {
-            target: { value },
-        } = event;
-        setNweet(value);
-    };
+  const onChange = (event) => {
+    event.preventDefault();
+    const {
+      target: { value },
+    } = event;
+    setNweet(value);
+  };
 
-      return (
+  //파일 읽기
+  const onFileChange = (event) => {
+    const {
+      target: {files},
+    } = (event);
+    const thefile = files[0];
+    const reader = new FileReader();
+    reader.onloadend = (finishedEvent) => {
+      const {
+        currentTarget: { result },
+      } = finishedEvent;
+      setAttachment(result);
+    }
+    reader.readAsDataURL(thefile);
+  }
+
+  //파일 선택 취소
+  const onClearAttachment = () => setAttachment("");
+
+  return (
     <>
       <div>
         <form onSubmit={onSubmit}>
@@ -80,7 +99,16 @@ const Home = ({ userObj }) => {
             onChange={onChange}
             value={nweet}
           />
+          {/* 파일 첨부, 삭제*/}
+          <input type="file" accept="image/*" onChange={onFileChange} />
+          {/* Nweet 작성 */}
           <input type="submit" value="Nweet" />
+          {attachment && (
+            <div>
+              <button onClick={onClearAttachment}>Clear</button>
+              <img src={attachment} width="50px" height="100px" />
+            </div>
+          )}
         </form>
           <div>
             {/* 트윗이 하나도 없을 경우 */}
@@ -92,7 +120,6 @@ const Home = ({ userObj }) => {
            }
         </div>
       </div>
-      
     </>
   );
 };
